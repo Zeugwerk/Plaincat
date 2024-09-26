7a4b55919670532e5ba174bf8b63e265442cb6d1
Donkey <iadonkey@zeugwerk.at>
2024-09-26 10:32:26 +0200

    fix: encode option does not work

--- Plaincat/Program.cs
+++ Plaincat/Program.cs
@@ -2,7 +2,7 @@
 using Plaincat;
 var transcoder = new Plaincat.Transcoder.Twincat();
 
-Parser.Default.ParseArguments(args, new[] { typeof(DecodeOptions), typeof(ReencodeOptions) })
+Parser.Default.ParseArguments(args, new[] { typeof(DecodeOptions), typeof(EncodeOptions), typeof(ReencodeOptions) })
     .WithParsed<DecodeOptions>((opts) => transcoder.Decode(opts.SourcePlcProj, opts.TargetPath))
     .WithParsed<EncodeOptions>((opts) => transcoder.Encode(opts.SourcePath, opts.TargetPath))
     .WithParsed<ReencodeOptions>((opts) =>
